d98ab8d12c801e8caf37b9f5ca07b8b5a5580f62
jorneylm <jorney95@live.com>
2021-08-14 16:17:47 -0600

    Final commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,27 @@ var sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 
 
- function css(){
-     return gulp
+function css() {
+    return gulp
         .src('scss/app.scss')
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 2 versions'],
             cascade: false
         }))
         .pipe(sass({
-            outputStyle:'expanded', //nested, compact, compressed
+            outputStyle: 'expanded', //nested, compact, compressed
         }))
         .pipe(gulp.dest('css'));
- }
+}
 
- function watchFiles(){
-     gulp.watch('scss/*.scss', css);
-    //  gulp.watch('index.html');
- }
+function watchFiles() {
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('index.html');
+}
 
 
- //registrar funciones como tareas
- //parallel carga paralela de archivos
- gulp.task('css', css);
- gulp.task('watch', gulp.parallel(watchFiles));
- 
+//registrar funciones como tareas
+//parallel carga paralela de archivos
+gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFiles));
+
